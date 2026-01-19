90ff78513e74d27d792e94a9a2ec3e82ddba5d8b
Francisco Bermejo Melero <franbermejo.dam@gmail.com>
2026-01-19 19:56:32 +0100

    Mejora de SEO y GEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://web3-blog-fran.vercel.app',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
